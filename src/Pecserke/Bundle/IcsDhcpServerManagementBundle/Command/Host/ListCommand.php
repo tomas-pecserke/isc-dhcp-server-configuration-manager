59e6241e20a39d55550c509094842a0c51802768
Tomáš Pecsérke <tomas.pecserke@gmail.com>
2016-04-21 13:36:51 +0200

    changed command to services

--- src/Pecserke/Bundle/IcsDhcpServerManagementBundle/Command/Host/ListCommand.php
+++ src/Pecserke/Bundle/IcsDhcpServerManagementBundle/Command/Host/ListCommand.php
@@ -1,25 +1,33 @@
 <?php
 namespace Pecserke\Bundle\IcsDhcpServerManagementBundle\Command\Host;
 
+use Pecserke\Bundle\IcsDhcpServerManagementBundle\Repository\HostRepository;
 use Pecserke\Component\IcsDhcpServer\Configuration\Host;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ListCommand extends ContainerAwareCommand {
+class ListCommand extends Command {
+    /**
+     * @var HostRepository
+     */
+    private $repository;
+
+    public function __construct(HostRepository $repository) {
+        parent::__construct();
+        $this->repository = $repository;
+    }
+
     protected function configure() {
         $this->setName('host:list');
         $this->setDescription('List static hosts');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $hosts = $this->getContainer()->get('pecserke_ics_dhcp_server_management.repository.host')->getHosts();
-
         $table = new Table($output);
         $table->setHeaders(['Name', 'HW Type', 'HW Address', 'Fixed Address', 'DDNS Hostname']);
-        $table->setRows(array_map([$this, 'prepareHostData'], $hosts));
+        $table->setRows(array_map([$this, 'prepareHostData'], $this->repository->getHosts()));
         $table->render();
     }
 
--- src/Pecserke/Bundle/IcsDhcpServerManagementBundle/Command/Lease/ListCommand.php
+++ src/Pecserke/Bundle/IcsDhcpServerManagementBundle/Command/Lease/ListCommand.php
@@ -1,14 +1,29 @@
 <?php
 namespace Pecserke\Bundle\IcsDhcpServerManagementBundle\Command\Lease;
 
+use Pecserke\Bundle\IcsDhcpServerManagementBundle\Repository\LeaseRepository;
 use Pecserke\Component\IcsDhcpServer\Lease\Lease;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ListCommand extends ContainerAwareCommand {
+class ListCommand extends Command {
     const DATE_FORMAT = 'Y-m-j H:i:s';
+
+    /**
+     * @var LeaseRepository
+     */
+    private $repository;
+
+    /**
+     * ListCommand constructor.
+     * @param LeaseRepository $repository
+     */
+    public function __construct(LeaseRepository $repository) {
+        parent::__construct();
+        $this->repository = $repository;
+    }
 
     protected function configure() {
         $this->setName('lease:list');
@@ -16,11 +31,9 @@ class ListCommand extends ContainerAwareCommand {
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $leases = $this->getContainer()->get('pecserke_ics_dhcp_server_management.repository.lease')->getNonFreeLeases();
-
         $table = new Table($output);
         $table->setHeaders(['IP', 'Hostname', 'Expires', 'State']);
-        $table->setRows(array_map([$this, 'prepareTableData'], $leases));
+        $table->setRows(array_map([$this, 'prepareTableData'], $this->repository->getNonFreeLeases()));
         $table->render();
     }
 
